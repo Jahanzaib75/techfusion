7aa21bd26d334ec37dfc92d376fc5613d8b217b9
jahanzaib <jahanzaibmetait@gmail.com>
2024-08-17 01:04:00 +0500

    changes

--- app/Models/qoute.php
+++ app/Models/qoute.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class qoute extends Model
+class Qoute extends Model
 {
     use HasFactory;
     protected $fillable=['firstname','lastname','contact','email','detail',];
